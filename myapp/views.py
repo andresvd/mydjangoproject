4708a9218720311564417e32a4b62ae98f550725
André van Drunen <aasvd@cin.ufpe.br>
2016-03-06 20:00:48 -0300

    added css and a little logic to the html

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import List
+from django.utils import timezone
+
 
 def index(request):
-    return render(request, 'myapp/index.html')
+    posts = List.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request, 'myapp/index.html', {'posts': posts})
+
 # Create your views here.
